0727bacdc7848e21b2865d3c5a9c047351f94ffe
Andres Olivares <andoli@chromium.org>
2024-02-15 18:44:02 +0100

    Send data for NTP clock sync

--- content_script.js
+++ content_script.js
@@ -1,8 +1,17 @@
 
 async function sendFetchData(sendResponse) {
+    const clientStartForClockSync = performance.now() + performance.timeOrigin;
     const response = await fetch('/api/timings', {cache: "no-store"});
+    const clientEndForClockSync = performance.now() + performance.timeOrigin;
     const jsonData = await response.json();
-    sendResponse({command: 'FETCH_RESPONSE', data: jsonData});
+    const {serverStartForClockSync, serverEndForClockSync} = jsonData;
+    // Naive NTP based on https://en.wikipedia.org/wiki/Network_Time_Protocol#Clock_synchronization_algorithm
+    const roundtripTime = clientEndForClockSync - clientStartForClockSync;
+    const serverProcessingTime = serverEndForClockSync - serverStartForClockSync;
+    const averageNetworkDelay = (roundtripTime - serverProcessingTime) / 2;
+    const serverClientTimeOffset = serverStartForClockSync - clientStartForClockSync - averageNetworkDelay;
+    const timingData = {timeOrigin: jsonData.timeOrigin + serverClientTimeOffset, timings: jsonData.timings};
+    sendResponse({command: 'FETCH_RESPONSE', data: timingData});
 }
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if(message.command === 'FETCH_DATA') {
