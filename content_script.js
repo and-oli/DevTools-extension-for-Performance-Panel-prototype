398e88865b185c662c3f7649238e2e70ff159f78
Andres Olivares <andoli@chromium.org>
2024-02-15 21:08:41 +0100

    Don't sync clocks

--- content_script.js
+++ content_script.js
@@ -1,15 +1,7 @@
 
 async function sendFetchData(sendResponse) {
-    const clientStartForClockSync = performance.now() + performance.timeOrigin;
     const response = await fetch('/api/timings', {cache: "no-store"});
-    const clientEndForClockSync = performance.now() + performance.timeOrigin;
     const jsonData = await response.json();
-    const {serverStartForClockSync, serverEndForClockSync} = jsonData;
-    // Naive NTP based on https://en.wikipedia.org/wiki/Network_Time_Protocol#Clock_synchronization_algorithm
-    const roundtripTime = clientEndForClockSync - clientStartForClockSync;
-    const serverProcessingTime = serverEndForClockSync - serverStartForClockSync;
-    const averageNetworkDelay = (roundtripTime - serverProcessingTime) / 2;
-    const serverClientTimeOffset = serverStartForClockSync - clientStartForClockSync - averageNetworkDelay;
     const timingData = {timeOrigin: jsonData.timeOrigin, timings: jsonData.timings};
     sendResponse({command: 'FETCH_RESPONSE', data: timingData});
 }
